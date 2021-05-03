cd8be5d2c928f9cb748c9938c2a2f19b6ac3cfbc
hota1024 <hotalog@hota1024.com>
2021-05-04 00:53:52 +0900

    🐛 fix loop file save dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import 'codemirror/theme/material.css'
 import { UnControlled as CodeMirror } from 'react-codemirror2'
@@ -10,22 +10,30 @@ import { ipcRenderer } from 'electron'
 const Hello = () => {
   const [value, setValue] = useState('<h1>I ♥ react-codemirror2</h1>')
 
-  ipcRenderer.on('start-file-save', async (_event, _arg) => {
-    const retval = await ipcRenderer.invoke('file-save', {
-      text: value,
-    })
-  })
+  useEffect(() => {
+    const onStartFileSave = async () => {
+      await ipcRenderer.invoke('file-save', {
+        text: value,
+      })
+    }
+
+    ipcRenderer.on('start-file-save', onStartFileSave)
+
+    return () => {
+      ipcRenderer.removeListener('start-file-save', onStartFileSave)
+    }
+  }, [value])
 
   return (
     <CodeMirror
-      value="<h1>I ♥ react-codemirror2</h1>"
+      value={value}
       options={{
         mode: 'markdown',
         theme: 'material',
         lineNumbers: true,
       }}
-      onChange={(_editor, _data, value) => {
-        setValue(value)
+      onChange={(_editor, _data, newValue) => {
+        setValue(newValue)
       }}
     />
   )
